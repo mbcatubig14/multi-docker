6d94782cd68dc4b3b08be77bcaaac1b4a9e3665e
Muhammad Catubig <mbcatubig14@gmail.com>
2021-01-24 11:15:21 +0000

    Push and Deploy to AWS

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,4 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders without crashing', () => {
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
+test('renders learn react link', () => {});
